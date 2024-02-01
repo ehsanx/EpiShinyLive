7b844c29359b7c427fceb17d04e10e97fc50c30a
Ehsan Karim <ehsanx@users.noreply.github.com>
2024-02-01 12:20:46 -0800

    path

--- compile.R
+++ compile.R
@@ -5,6 +5,6 @@ output_path <- paste0(getwd(), "/docs")
 
 # Use the paths in the export function
 shinylive::export(app_path, output_path)
-httpuv::runStaticServer("E:/GitHub/EpiShinyLiveW/docs")
-#servr::httd("E:/GitHub/EpiShinyLive/docs")
+httpuv::runStaticServer(paste0(getwd(), "/docs"))
+servr::httd(paste0(getwd(), "/docs"))
 
